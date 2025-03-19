81222e604d421b8cbbb583355d4eef97d9f419e7
IvanPrudey <prudey.ia@yandex.ru>
2025-03-20 00:19:22 +0300

    correct_api_views_and_add_serializers

---
+++ backend/foodgram/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework.serializers import ModelSerializer
+
+from recipes.models import Ingredient, Recipe, Tag
+
+
+class IngredientSerializer(ModelSerializer):
+
+    class Meta:
+        model = Ingredient
+        fields = (
+            'id',
+            'name',
+            'measurement_unit',
+        )
+
+
+class TagSerializer(ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = (
+            'id',
+            'name',
+            'color',
+            'slug',
+        )
+
+
+class RecipeSerializer(ModelSerializer):
+
+    class Meta:
+        model = Recipe
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,14 +2,19 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from recipes.models import Ingredient, Recipe, Tag
 
+from .serializers import IngredientSerializer, RecipeSerializer, TagSerializer
+
 
 class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
 
 
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
 
 
 class TagViewSet(ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
+    serializer_class = TagSerializer
