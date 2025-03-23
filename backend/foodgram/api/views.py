595be29b668844d4f6652f12ff13ac97ed0f2ff3
IvanPrudey <prudey.ia@yandex.ru>
2025-03-23 17:37:01 +0300

    add_permissions

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,22 +1,25 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from recipes.models import Ingredient, Recipe, Tag
-
 from api.serializers import (IngredientSerializer,
                              RecipeSerializer,
                              TagSerializer)
+from api.permissions import IsAdminAuthorOrReadOnly, IsAdminOrReadOnly 
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    permission_classes = IsAdminOrReadOnly
 
 
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    permission_classes = IsAdminAuthorOrReadOnly
 
 
 class TagViewSet(ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = IsAdminOrReadOnly
