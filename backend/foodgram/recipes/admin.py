69763a0fc71aaa6412e4fe22c7a59af33f47022e
IvanPrudey <prudey.ia@yandex.ru>
2025-03-23 11:22:17 +0300

    correct_admin_zone_2

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -5,44 +5,37 @@ from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     """Админ панель для модели Ingredient."""
+
     list_display = ('name', 'measurement_unit')
     search_fields = ('name',)
     list_filter = ('measurement_unit',)
     empty_value_display = '-пусто-'
 
-    class Meta:
-        verbose_name = 'ингридиент'
-        verbose_name_plural = 'Ингридиенты'
-
 
 @admin.register(IngredientInRecipe)
 class IngredientInRecipeAdmin(admin.ModelAdmin):
     """Админ панель для модели IngredientInRecipe."""
+
     list_display = ('ingredient', 'amount')
     search_fields = ('ingredient__name',)
     list_filter = ('ingredient',)
     empty_value_display = '-пусто-'
 
-    class Meta:
-        verbose_name = 'ингридиент в рецепте'
-        verbose_name_plural = 'Ингридиенты в рецепте'
-
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     """Админ панель для модели Tag."""
+
     list_display = ('name', 'color', 'slug')
     search_fields = ('name', 'slug')
     list_filter = ('color',)
     empty_value_display = '-пусто-'
 
-    class Meta:
-        verbose_name = 'тег'
-        verbose_name_plural = 'Теги'
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """Админ панель для модели Recipe."""
+
     list_display = (
         'name', 'author', 'cooking_time', 'pub_date'
     )
@@ -50,7 +43,3 @@ class RecipeAdmin(admin.ModelAdmin):
     list_filter = ('author', 'tags', 'pub_date')
     filter_horizontal = ('ingredients', 'tags')
     empty_value_display = '-пусто-'
-
-    class Meta:
-        verbose_name = 'рецепт'
-        verbose_name_plural = 'Рецепты'
