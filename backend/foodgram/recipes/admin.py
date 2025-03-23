943a084ce541249a07e64c21149f1e3fea54b94d
IvanPrudey <prudey.ia@yandex.ru>
2025-03-23 10:15:30 +0300

    correct_admin_zone

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
 
-# Register your models here.
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """Админ панель для модели Ingredient."""
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('measurement_unit',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    """Админ панель для модели IngredientInRecipe."""
+    list_display = ('ingredient', 'amount')
+    search_fields = ('ingredient__name',)
+    list_filter = ('ingredient',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Админ панель для модели Tag."""
+    list_display = ('name', 'color', 'slug')
+    search_fields = ('name', 'slug')
+    list_filter = ('color',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """Админ панель для модели Recipe."""
+    list_display = (
+        'name', 'author', 'cooking_time', 'pub_date'
+    )
+    search_fields = ('name', 'author__username', 'tags__name')
+    list_filter = ('author', 'tags', 'pub_date')
+    filter_horizontal = ('ingredients', 'tags')
+    empty_value_display = '-пусто-'
