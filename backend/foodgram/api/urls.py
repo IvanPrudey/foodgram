449eba2419f84d67060138b989e268f927e7f7fc
IvanPrudey <prudey.ia@yandex.ru>
2025-03-30 13:23:11 +0300

    correct_all_urls_with_vers

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,17 +1,20 @@
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
+from users.views import UserViewSet
 
 app_name = 'api'
 
-router = DefaultRouter()
-
-router.register('ingredients', IngredientViewSet)
-router.register('tags', TagViewSet)
-router.register('recipes', RecipeViewSet)
+router_version_1 = DefaultRouter()
+router_version_1.register(r'users', UserViewSet, basename='users')
+router_version_1.register(r'ingredients', IngredientViewSet, basename='ingredients')
+router_version_1.register(r'tags', TagViewSet, basename='tags')
+router_version_1.register(r'recipes', RecipeViewSet, basename='recipes')
+router_version_1.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router_version_1.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/auth/', include('djoser.urls.authtoken')),
 ]
--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -3,7 +3,53 @@ from base64 import b64decode
 from django.core.files.base import ContentFile
 from rest_framework import serializers
 
-from users.models import User
+from djoser.serializers import UserCreateSerializer, UserSerializer
+
+from users.models import User, Subscription
+
+
+class CustomUserCreateSerializer(UserCreateSerializer):
+    """Сериализатор для создания пользователя."""
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+        ]
+
+
+class CustomUserSerializer(UserSerializer):
+    """
+    Сериализатор для отображения информации
+    о пользователе с проверкой подписки.
+    """
+
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+        ]
+
+    def get_is_subscribed(self, obj):
+        """Проверка наличия подписки."""
+        request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
+        return Subscription.objects.filter(
+            user=request.user,
+            author=obj
+        ).exists()
 
 
 class Base64ImageField(serializers.ImageField):
