bb4f9bd4cc5d6f42a6232495f79ff6418e0049bf
IvanPrudey <prudey.ia@yandex.ru>
2025-04-24 15:12:13 +0300

    correct_docker-compose.production

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'foodgram/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
