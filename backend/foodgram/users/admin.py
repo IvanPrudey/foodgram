3120596b6ca40239289b3cbc1e2a3db6541a7159
IvanPrudey <prudey.ia@yandex.ru>
2025-04-28 17:50:50 +0300

    add_in_admin_zone_count_recipes_and_subscribers

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.db.models import Count
 
 from users.models import User, Subscription
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     """Админ-панель для управления пользователями."""
 
     list_display = (
@@ -12,13 +14,33 @@ class UserAdmin(admin.ModelAdmin):
         'email',
         'first_name',
         'last_name',
-        'avatar'
+        'avatar',
+        'get_recipe_count',
+        'get_subscriber_count',
     )
     list_display_links = ('email',)
     list_filter = ('email', 'username')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username',)
     readonly_fields = ('date_joined', 'last_login')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            recipe_count=Count('recipes', distinct=True),
+            subscriber_count=Count('subscribers', distinct=True),
+        )
+        return queryset
+
+    def get_recipe_count(self, obj):
+        return obj.recipe_count
+    get_recipe_count.short_description = 'Рецептов'
+    get_recipe_count.admin_order_field = 'recipe_count'
+
+    def get_subscriber_count(self, obj):
+        return obj.subscriber_count
+    get_subscriber_count.short_description = 'Подписчиков'
+    get_subscriber_count.admin_order_field = 'subscriber_count'
 
     class Meta:
         verbose_name = 'Пользователь'
