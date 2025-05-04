40ae1824fc06ceb5b2abe72b1bff16da49e3ea4f
IvanPrudey <prudey.ia@yandex.ru>
2025-05-04 20:35:44 +0300

    correct_load_data_script_with_Path(__file__).resolve().parents[3]

--- backend/recipes/management/commands/load_data_script.py
+++ backend/recipes/management/commands/load_data_script.py
@@ -1,11 +1,8 @@
 import csv
-
+from pathlib import Path
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
-
-
-PATH_CSV_FILES = 'data/'
 
 
 class Command(BaseCommand):
@@ -13,18 +10,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.load_ingredients()
-        print('Loading of ingredients is completed')
+        self.stdout.write(
+            self.style.SUCCESS('Loading of ingredients is completed')
+        )
 
     def load_ingredients(self):
-        Ingredient.objects.all().delete()
-        with open(
-            f'{PATH_CSV_FILES}ingredients.csv', 'r', encoding='utf-8'
-        ) as file:
+        csv_path = (
+            Path(__file__).resolve().parents[3]
+            / 'data'
+            / 'ingredients.csv'
+        )
+        if not csv_path.exists():
+            self.stdout.write(
+                self.style.ERROR(f'File {csv_path} not found!')
+            )
+            return
+
+        with open(csv_path, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 Ingredient.objects.get_or_create(
                     name=row['name'],
-                    defaults={
-                        'measurement_unit': row['measurement_unit']
-                    }
+                    measurement_unit=row['measurement_unit']
                 )
