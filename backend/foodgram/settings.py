f5cd8bc3ba445dde05ad4707a63c6db395c734c6
IvanPrudey <prudey.ia@yandex.ru>
2025-07-25 19:52:03 +0300

    refresh_actions_secrets

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -171,6 +171,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://profoodgramchik.hopto.org'
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://profoodgramchik.hopto.org'
+]
